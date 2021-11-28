631545ff7d87cccdb00d7d22053a3a8ab763207b
Cam <camerontate920@yahoo.com>
2021-11-27 19:11:37 -0500

    first commit commit 1st

--- main.go
+++ main.go
@@ -2,21 +2,12 @@ package main
 
 import (
 	"fmt"
-	"html"
-	"log"
-	"net/http"
+
+	"rsc.io/quote"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
-
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi")
-	})
-
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	fmt.Println(quote.Go())
 
 }
